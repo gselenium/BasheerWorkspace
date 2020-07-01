c23023b84de20a9965b6af9b616176f81149072f
Govardhan Karnati <gkarnati-c@g2llc.com>
2020-07-01 23:24:30 +0530

    Created new github maven project

---
+++ src/main/java/com/selenium/BasicJavaProgram.java
@@ -0,0 +1,12 @@
+package com.selenium;
+
+public class BasicJavaProgram {
+	//Create or add main method:
+	//type main and press control+Space bar
+	public static void main(String[] args) {
+		//insert printf or systemoutprintln: 
+		//type syso and press control+space bar
+		System.out.println("Basic Java program");
+	}
+
+}
---
+++ src/main/java/com/selenium/VariablesDeclaration.java
@@ -0,0 +1,33 @@
+package com.selenium;
+
+public class VariablesDeclaration {
+	public static void main(String[] args) {
+		//Declare the numbers in Java we can use int  //32bytes
+		int a;
+		a=345345455;
+		int a1=12;
+		int a2,a3,a4;
+		int a5=45,a6=78;
+		a=45;
+		
+		System.out.println(a);
+		
+		System.out.println(a);
+		//it can store the numbers or numeric values;  64 bytes
+		long l=2132334444;
+		System.out.println(l);
+		l=34;
+		long l2=56;
+		System.out.println(l2);
+		
+		//in java to store the decimal numbers we can use double:
+		double d=23.45;
+		System.out.println(d);
+		double d1=65.5;
+		System.out.println(d1);
+		
+		double d2=75;
+		System.out.println(d2);		
+	}
+
+}
